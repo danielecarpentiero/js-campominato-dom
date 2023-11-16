c3dcf906d67b15badc20e7bf236f8bdb693c65b2
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-16 18:52:57 +0100

    feat: scegliere difficoltà

--- js/main.js
+++ js/main.js
@@ -9,33 +9,41 @@ const buttonToggle = document.querySelector(".play");
 
 buttonToggle.addEventListener("click", function () {
   toggleProperty(boxAdder, "hidden");
+  boxAdder.innerHTML = "";
+
+  const myDiff = document.getElementById("difficolta");
+  console.log(myDiff.value);
+
+  let level = "";
+  let totCells = 0;
+  switch (myDiff.value) {
+    case "medio":
+      level = "medium";
+      totCells = 81;
+      break;
+    case "difficile":
+      level = "hard";
+      totCells = 49;
+      break;
+    case "facile":
+      level = "easy";
+    default:
+      totCells = 100;
+      break;
+  }
+
+  for (let i = 1; i < totCells + 1; i++) {
+    const boxes = document.createElement("div");
+    boxes.classList.add("box");
+    boxes.classList.add("flex");
+    boxes.classList.add(level);
+    boxes.innerHTML = i;
+    boxAdder.append(boxes);
+    boxes.addEventListener("click", function () {
+      toggleProperty(boxes, "color");
+      console.log(`hai cliccato sulla casella numero ${i}`);
+    });
+  }
 });
 
 /* casella diventa azzurra al click */
-const myDiff = document.getElementById("difficolta").value;
-for (let i = 0; i < myDiff; i++) {
-  const boxes = document.createElement("div");
-  boxes.classList.add("box");
-  boxes.classList.add("flex");
-  boxes.innerHTML = i + 1;
-  boxAdder.append(boxes);
-  boxes.addEventListener("click", function () {
-    toggleProperty(boxes, "color");
-    console.log(`hai cliccato sulla casella numero ${i}`);
-  });
-  switch (myDiff) {
-    case "medio":
-      boxes.classList.add("medium");
-      i = 81;
-      break;
-    case "difficile":
-      boxes.classList.add("hard");
-      i = 49;
-      break;
-    case "facile":
-    default:
-      boxes.classList.add("easy");
-      i = 100;
-      break;
-  }
-}
